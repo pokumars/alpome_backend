de2d5a06c708b754bc428056898a92f633f815b7
oheneba <ohenebakob@hotmail.com>
2020-10-22 02:14:27 +0300

    serve users and single user

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   'rules': {
     'indent': [
-      'error',
+      'warn',
       2
     ],
     'linebreak-style': [
--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const cors = require('cors');
+const logger = require('./utils/logger');
+
 
 const app = express();
-app.use(cors);
+app.use(cors());
 
-
+const persons = require('./scratchpad').persons;
 app.get('/api', (request, response, next) => {
-  response.send('we have lift off');
+  response.send(`
+  <p>This is probably not what you are looking for. Try</p> 
+  <a href="/api/users">/api/users</a> <br/>
+  <a href="/api/users/2">/api/users/2</a>
+  `);
+});
+
+app.get('/api/users', (request, response, next) => {
+  response.send(persons);
+});
+
+app.get('/api/users/:id', (request, response, next) => {
+  const id = request.params.id;
+  console.log(id);
+  const user = persons.find(user => Number(user.id) === Number(id));
+  response.send(user);
 });
 
 const PORT = 3002;
 app.listen(PORT,() => {
-  console.log(`server is running on port ${PORT}`);
+  logger.info(`server is running on port ${PORT}`);
 });
+
+//we will need to serve users
+//we will need to serve growing units/plants
+  //images, the users who have access
