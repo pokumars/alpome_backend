f226863c5d7f6e69e7aab784dc0b5e68b4357a3b
oheneba <ohenebakob@hotmail.com>
2020-10-30 11:55:38 +0200

    add schema for users and growing units

---
+++ models/growing_units.js
@@ -0,0 +1,35 @@
+const mongoose= require('mongoose');
+
+
+
+const growingUnitSchema = new mongoose.Schema({
+  /*scientific_details_trefle: {
+    id: String,
+    scientific_name: String
+  },*/
+  nickname: String,
+  location: String,
+  supragarden: Boolean,
+  last_watered: Date, //null if supragarden,
+  watering_frequency: Number,
+  data_source: String,
+  common_names: [String],
+  owner: String,
+  shared_access: [String],
+  stream_url: String,
+  images: [{image_url: String, date_uploaded: Date, }]
+});
+
+
+growingUnitSchema.set('toJSON', {
+  transform:(document, returnedObject) => {
+    returnedObject.unit_id = returnedObject._id.toString();
+
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
+
+//https://mongoosejs.com/docs/models.html
+module.exports = mongoose.model('Growing Unit', growingUnitSchema);
