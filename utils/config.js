9691b69db0e6d48efaa38c5dcc6bffb658acef0a
oheneba <ohenebakob@hotmail.com>
2020-11-02 13:12:20 +0200

    add environment variables to config file

--- utils/config.js
+++ utils/config.js
@@ -4,8 +4,17 @@ let MONGODB_URI = process.env.MONGO_DB_URI;
 let MONGO_DB_TRIAL_URI = process.env.MONGO_DB_TRIAL_URI;
 let PORT = process.env.PORT;
 
+let AWS_ID = process.env.AWS_ID;
+let AWS_SECRET = process.env.AWS_SECRET;
+let AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME;
+
+
+//TODO: add the other environment keys
 module.exports = {
   MONGO_DB_TRIAL_URI,
   MONGODB_URI,
-  PORT
+  PORT,
+  AWS_ID,
+  AWS_SECRET,
+  AWS_BUCKET_NAME,
 };
