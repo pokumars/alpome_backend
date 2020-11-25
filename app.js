d1f67a8ecbcc24a38eb24791fa7736e1946b6cdc
oheneba <ohenebakob@hotmail.com>
2020-11-26 01:07:08 +0200

    add Procfile and build directory

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(middleware.getTokenFrom);
-
+app.use(express.static('build'));
 
 if (process.env.NODE_ENV !== 'test'){
   //morgan logs any incoming request. The array below is  how we want it to log the details
